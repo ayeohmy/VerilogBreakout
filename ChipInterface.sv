1ed93256625aa5432cda4eab3afa88ecd7c71309
Audrey 'Yo <ayeohmy@gmail.com>
2014-11-04 17:17:01 -0500

    Added the brick module and colour module so that colours selected. To add: Ball and collision detection

--- ChipInterface.sv
+++ ChipInterface.sv
@@ -57,9 +57,135 @@ module checkButton
 
 endmodule: checkButton
 
+/////////////////////////// COLOUR MODULE /////////////////////////////
+
+module colour
+	(input logic CLOCK_50, reset
+	 input logic [8:0] row,
+	 input logic [9:0] col,
+	output logic [7:0] red, green, blue);
+
+	logic [4:0] brick; // which brick is being looked at?
+	logic paddle, wall;
+	// logic ball;											// TODO~
+
+	bricks BR (CLOCK_50, reset, row, col, brick);
+	paddle PAD (CLOCK_50, reset, row, col, paddle);
+	wall WAL (CLOCK_50, reset, row, col, wall);
+	// ball BAL (CLOCK_50, reset, row, col, ball);			// TODO~
+
+	always_comb begin
+		if (wall) begin
+			red = 8'hCC;
+			green = 8'hCC;
+			blue = 8'hCC;
+		end
+		else if (brick) begin
+			if(brick[0]) begin
+				red = 8'hFF;
+				green = 8'hFF;
+				blue = 8'h00;
+			end
+			else begin
+				red = 8'hFF;
+				green = 8'h00;
+				blue = 8'hFF;
+			end
+		end
+		else if (paddle) begin
+			red = 8'h00;
+			green = 8'hFF;
+			blue = 8'h00;
+		end
+		// else if (ball) begin								// TODO~
+
+		// end
+		else begin
+			red = 8'hFF;
+			green = 8hFF;
+			blue = 8'hFF;
+		end
+
+	end
+
+endmodule: colour
 
 /////////////////////////// OBJECT MODULE /////////////////////////////
 
+// This module instantiates all the bricks
+// It returns which brick it's on, if there is a brick
+// No brick = 0 | brokenBrick = 0 | topRow = 1 2 3 4 5 6 | bottomRow = 8 9 10 11 12 13
+module bricks
+	(input logic CLOCK_50, reset
+	 input logic [8:0] row,
+	 input logic [9:0] col,
+	output logic [4:0] brick);
+
+	logic brick0, brick1, brick2, brick3, brick4, brick5, brick6, brick7, brick8, brick9, brickA, brickB;
+
+	// top row 
+	brick #( 40, 100, 100, 30) b0(CLOCK_50, reset, row, col, brick0); 
+	brick #(140, 100, 100, 30) b1(CLOCK_50, reset, row, col, brick1); 
+	brick #(240, 100, 100, 30) b2(CLOCK_50, reset, row, col, brick2); 
+	brick #(340, 100, 100, 30) b3(CLOCK_50, reset, row, col, brick3); 
+	brick #(440, 100, 100, 30) b4(CLOCK_50, reset, row, col, brick4); 
+	brick #(540, 100, 50, 30) b5(CLOCK_50, reset, row, col, brick5); 
+	// 2nd row 
+	brick #( 40, 150, 50, 30) b6(CLOCK_50, reset, row, col, brick6); 
+	brick #( 90, 150, 100, 30) b7(CLOCK_50, reset, row, col, brick7); 
+	brick #(190, 150, 100, 30) b8(CLOCK_50, reset, row, col, brick8); 
+	brick #(290, 150, 100, 30) b9(CLOCK_50, reset, row, col, brick9); 
+	brick #(390, 150, 100, 30) bA(CLOCK_50, reset, row, col, brickA); 
+	brick #(490, 150, 100, 30) bB(CLOCK_50, reset, row, col, brickB); 
+
+	always_comb begin
+		if(brick0)
+			brick = 1;
+		if(brick1)
+			brick = 2;
+		if(brick2) 
+			brick = 3;
+		if(brick3)
+			brick = 4;
+		if(brick4)
+			brick = 5;
+		if(brick5)
+			brick = 6;
+		if(brick6) 
+			brick = 8;
+		if(brick7)
+			brick = 9;
+		if(brick8)
+			brick = 10;
+		if(brick9)
+			brick = 11;
+		if(brickA)
+			brick = 12;
+		if(brickB)
+			brick = 13;
+	end
+endmodule: bricks
+
+module brick
+#(parameter LEFT = 40, TOP = 100, WIDTH = 100, HEIGHT = 30)
+	(input logic CLOCK_50, reset
+	 input logic [8:0] row,
+	 input logic [9:0] col,
+	output logic signal);
+
+	logic withinRow, withinColumn;
+
+	offset_check row (row, LEFT, WIDTH, withinRow);
+	offset_check col (col, TOP, HEIGHT, withinColumn);
+
+	// assign withinRow = (row >= LEFT && row <= (LEFT + WIDTH));
+	// assign withinColumn = (col >= TOP && col <= (TOP + HEIGHT));
+
+	assign signal = withinRow && withinColumn;
+
+endmodule: brick
+
+// This module checks whether a wall should be at the given row and column and outputs a signal
 module wall
 	(input logic CLOCK_50, reset
 	 input logic [8:0] row,
@@ -67,16 +193,32 @@ module wall
 	output logic signal);
 
 	logic leftWall, rightWall, topWall;
+	logic leftWallRow, leftWallCol;
+	logic rightWallRow, rightWallCol;
+	logic topWallRow, topWallCol;
 
-	assign leftWall = (col >= 20 && col <= 39) && (row >= 10 && row <=469);
-	assign rightWall = (col >= 590 && col <= 609) && (row >= 10 && row <=469);
-	assign topWall = (row >= 10 && row <= 29) && (col >= 20 && col <= 609);
+	range_check leftRow (row, 10, 469, leftWallRow);
+	range_check leftCol (col, 20, 39, leftWallCol);
+	range_check rightRow (row, 10, 469, rightWallRow);
+	range_check rightCol (col, 590, 609, rightWallCol);
+	range_check topRow (row, 10, 29, topWallRow);
+	range_check topCol (col, 20, 609, topWallCol);
+
+	assign leftWall = leftWallRow && leftWallCol;
+	assign rightWall = rightWallRow && rightWallCol;
+	assign topWall = topWallRow && topWallCol;
+
+	//assign leftWall = (col >= 20 && col <= 39) && (row >= 10 && row <=469);
+	//assign rightWall = (col >= 590 && col <= 609) && (row >= 10 && row <=469);
+	//assign topWall = (row >= 10 && row <= 29) && (col >= 20 && col <= 609);
 
 	assign signal = leftWall | rightWall | topWall;
 
 endmodule: wall
 
-
+// This module checks for whether the paddle should be present at the given row and col every game cycle 
+// Returns a 1 signal if the paddle should be there
+// ALSO, the module updates the position of the paddle each time
 module paddle
 	(input logic CLOCK_50, reset,
 	 input logic [8:0] row,
